70727a86c01c0059d826784b9dfdfbac6a18c652
Dokkan <suhyun1089@naver.com>
2023-04-22 15:08:54 +0900

    0422

--- src/main/java/idusw/springboot/boardksh/controller/HomeController.java
+++ src/main/java/idusw/springboot/boardksh/controller/HomeController.java
@@ -16,7 +16,6 @@ public class HomeController {
      */
     @GetMapping("/") //localhost:port로 요청을 하면 getAdmin() 메소드를 호출하여 처리하고 /admin/index view에게 전달
     public String getAdmin() {
-        System.out.println("getAdmin");
         return "/admin/index";
     }
 
